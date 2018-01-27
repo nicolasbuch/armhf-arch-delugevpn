129db0ed187d32c852d991cde6366f8f21000aa8
Nicolas Buch <nb@hoej.dk>
2018-01-27 22:29:14 +0100

    Added a few improvements

--- process-download.sh
+++ process-download.sh
@@ -3,15 +3,18 @@ torrent_id=$1
 torrent_name=$2
 torrent_dir=$3
 full_path="${torrent_dir}/${torrent_name}"
-tmp_output_dir="/storage/tmp/$torrent_name"
+tmp_output_dir="/mnt/storage/tmp/$torrent_name"
 tmp_output_ffmpeg_path="$tmp_output_dir/$torrent_name.mp4"
-final_output_dir="/storage/movies"
+final_output_dir="/mnt/storage"
 
 # Loop through all rar files and extract them
 for file in $(find $full_path -type f -name '*.rar')
 do
     unrar e -y $file
 done
+
+# Create temporary directory structure
+mkdir -p $tmp_output_dir
 
 # Convert all .mkv video files which are not named "sample" to mp4 with AC3 audio
 for mkv_path in $(find $full_path -type f -name '*.mkv' | grep -v 'sample')
@@ -27,4 +30,4 @@ do
 done
 
 # Rename files/folders and move them to movies folder
-filebot -script fn:amc --output "$final_output_dir" --action duplicate --conflict skip -non-strict --log-file "/mnt/286a017f-d80e-4041-8c5b-df787720eceb/amc.log" --def unsorted=y music=y artwork=y excludeList=".excludes" ut_dir="$full_path" ut_kind="multi" ut_title="$torrent_name" ut_label=""
+filebot -script fn:amc --output "$final_output_dir" --action move --conflict skip -non-strict --log-file "/mnt/storage/amc.log" --def unsorted=y excludeList=".excludes" ut_dir="$tmp_output_dir" ut_kind="multi" ut_title="$torrent_name" ut_label=""
